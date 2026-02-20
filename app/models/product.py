1016f860406239f209e85eb514488dec901ec8e0
Ismail <ismll.s@mail.ru>
2026-02-20 20:38:15 +0400

    feat: added attributes field to product model and commited with alembic

--- app/models/product.py
+++ app/models/product.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from sqlalchemy import DateTime, String, ForeignKey, func
+from sqlalchemy import String, ForeignKey, func
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
@@ -10,6 +11,8 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
     url: Mapped[str] = mapped_column(unique=True)
+
+    attributes: Mapped[dict] = mapped_column(JSONB, nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
