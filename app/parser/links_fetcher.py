02e8f45195f6dd26b7a19b10c63229d7a88aaf73
Ismail <ismll.s@mail.ru>
2026-02-20 14:10:02 +0400

    feat: created the parser structure and wrote links_fetcher.py

---
+++ app/parser/links_fetcher.py
@@ -0,0 +1,43 @@
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+SITEMAP_URL = "https://best-magazin.com/ocsitemap.xml"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+
+async def links_fetcher(url: str, session: aiohttp.ClientSession, limit: int = 10):
+    try:
+        async with session.get(url=url, headers=headers) as response:
+            if response.status == 200:
+                html = await response.text()
+                soup = BeautifulSoup(html, "xml")
+
+                locs = soup.find_all("loc")
+                links = [
+                    loc.text for loc in locs if "/apple/iphone/" in loc.text and loc.text.endswith(".html")]
+
+                return links[:limit]
+            else:
+                print(f"Ошибка сервера: {response.status}")
+                return []
+    except Exception as e:
+        print(f"Ошибка сети: {e}")
+        return []
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def test():
+        async with aiohttp.ClientSession() as session:
+            print(f"--- Тестовый запуск ---")
+            links = await links_fetcher(SITEMAP_URL, session)
+            print(f"Найдено ссылок: {len(links)}")
+            for l in links:
+                print(l)
+
+    asyncio.run(test())
