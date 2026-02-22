ef413787a1c13e2618c06926ea79a2cadf702b7b
Ismail <ismll.s@mail.ru>
2026-02-22 23:02:16 +0400

    refactor: use bulk upsert for products and update price history

--- app/parser/database_ops.py
+++ app/parser/database_ops.py
@@ -6,30 +6,42 @@ from app.models import Product, PriceHistory
 
 
 async def save_parsed_data(parsed_items: list[dict]):
-    """Gets list[dict] and save it to db (Upsert + Price History)"""
+    """Perform bulk upsert for products and record price history"""
+    if not parsed_items:
+        return
+
     async with async_session_maker() as session:
-        for item in parsed_items:
-            stmt = insert(Product).values(
-                name=item["name"],
-                url=item["url"],
-                attributes=item["attributes"]
+        products_data = [
+            {
+                "name": item["name"],
+                "url": item["url"],
+                "attributes": item["attributes"]
+            }
+            for item in parsed_items
+        ]
+
+        stmt = insert(Product).values(products_data)
+
+        do_update_stmt = stmt.on_conflict_do_update(
+            index_elements=["url"],
+            set_=dict(
+                attributes=stmt.excluded.attributes,
+                updated_at=func.now()
             )
+        ).returning(Product.id, Product.url)
 
-            do_update_stmt = stmt.on_conflict_do_update(
-                index_elements=["url"],
-                set_=dict(
-                    attributes=stmt.excluded.attributes,
-                    updated_at=func.now()
-                )
-            ).returning(Product.id)
+        result = await session.execute(do_update_stmt)
+        db_products = result.all()
 
-            result = await session.execute(do_update_stmt)
-            product_id = result.scalar_one()
+        url_id_to_map = {row.url: row.id for row in db_products}
 
-            new_price = PriceHistory(
-                product_id=product_id,
+        prices_data = [
+            PriceHistory(
+                product_id=url_id_to_map[item["url"]],
                 price=item["price"]
             )
-            session.add(new_price)
+            for item in parsed_items
+        ]
 
+        session.add_all(prices_data)
         await session.commit()
