a9b0e88e76bcf6aba092fee4fe1340accf9ad32a
Ismail <ismll.s@mail.ru>
2026-02-22 21:43:54 +0400

    feat: added status codes to endpoints

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_async_db
@@ -12,11 +12,11 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=list[ProductWithPrices])
+@router.get("/", response_model=list[ProductWithPrices], status_code=status.HTTP_200_OK)
 async def get_products(db: AsyncSession = Depends(get_async_db)):
     return await crud.get_all_products(db)
 
 
-@router.get("/{product_id}", response_model=ProductWithPrices)
+@router.get("/{product_id}", response_model=ProductWithPrices, status_code=status.HTTP_200_OK)
 async def get_product_by_id(product_id: int, db: AsyncSession = Depends(get_async_db)):
     return await crud.get_product_by_id(product_id, db)
