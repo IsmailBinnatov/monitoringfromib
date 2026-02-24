2d1b629d23fca3015a30e645f9d1cf6700f779a8
Ismail <ismll.s@mail.ru>
2026-02-24 21:24:39 +0400

    feat(api): implement delete routes for products and history management

--- app/api/routers/product.py
+++ app/api/routers/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_async_db
@@ -20,3 +20,38 @@ async def get_products(db: AsyncSession = Depends(get_async_db)):
 @router.get("/{product_id}", response_model=ProductWithPrices, status_code=status.HTTP_200_OK)
 async def get_product_by_id(product_id: int, db: AsyncSession = Depends(get_async_db)):
     return await crud.get_product_by_id(product_id, db)
+
+
+@router.delete("/all", status_code=status.HTTP_200_OK)
+async def delete_all_products(db: AsyncSession = Depends(get_async_db)):
+    await crud.delete_all_products(db)
+    return {"message": "All products deleted"}
+
+
+@router.delete("/{product_id}")
+async def delete_product_by_id(product_id: int, db: AsyncSession = Depends(get_async_db)):
+    is_deleted = await crud.delete_product_by_id(product_id, db)
+    if not is_deleted:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product not found"
+        )
+    return {"message": f"Product ID:{product_id} deleted"}
+
+
+@router.delete("/history/all", status_code=status.HTTP_200_OK)
+async def delete_all_price_history(db: AsyncSession = Depends(get_async_db)):
+    await crud.delete_all_price_history(db)
+    return {"message": "All product price history has been deleted"}
+
+
+@router.delete("/history/product/{product_id}", status_code=status.HTTP_200_OK)
+async def delete_history_by_product_id(product_id: int, db: AsyncSession = Depends(get_async_db)):
+    await crud.delete_history_by_product_id(product_id, db)
+    return {"message": f"History of Product ID: {product_id} has been deleted"}
+
+
+@router.delete("/history/{history_id}", status_code=status.HTTP_200_OK)
+async def delete_history_by_history_id(history_id: int, db: AsyncSession = Depends(get_async_db)):
+    await crud.delete_history_by_history_id(history_id, db)
+    return {"message": f"History ID: {history_id} has been deleted"}
--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
-from sqlalchemy import select
+from sqlalchemy import delete, select
 
-from app.models import Product
+from app.models import Product, PriceHistory
 
 
 async def get_all_products(db: AsyncSession):
@@ -17,3 +17,41 @@ async def get_product_by_id(product_id: int, db: AsyncSession):
         .where(Product.id == product_id))
     ).first()
     return product
+
+
+async def delete_all_products(db: AsyncSession):
+    await db.execute(delete(Product))
+    await db.commit()
+
+
+async def delete_product_by_id(product_id: int, db: AsyncSession):
+    product = await get_product_by_id(product_id, db)
+    if product:
+        await db.delete(product)
+        await db.commit()
+        return True
+    return False
+
+
+async def delete_all_price_history(db: AsyncSession):
+    await db.execute(delete(PriceHistory))
+    await db.commit()
+
+
+async def delete_history_by_product_id(product_id: int, db: AsyncSession):
+    stmt = delete(PriceHistory).where(PriceHistory.product_id == product_id)
+    await db.execute(stmt)
+    await db.commit()
+
+
+async def delete_history_by_history_id(history_id: int, db: AsyncSession):
+    price_history = (await db.scalars(
+        select(PriceHistory)
+        .where(PriceHistory.id == history_id))
+    ).first()
+
+    if price_history:
+        await db.delete(price_history)
+        await db.commit()
+    else:
+        print("There is nothing to delete.")
