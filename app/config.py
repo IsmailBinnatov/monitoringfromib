1e090c43d5d02c0a1c8b12f097adec5ba2ede0f2
Ismail <ismll.s@mail.ru>
2026-02-26 00:25:44 +0400

    feat: added jwt-env settings

--- app/config.py
+++ app/config.py
@@ -2,11 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # DB
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_NAME: str
     DB_PORT: int
+
+    # JWT
+    SECRET_KEY: str
+    ALGORITHM: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
